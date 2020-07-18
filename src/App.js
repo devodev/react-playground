404a35435374181d7d2b58823f71dec992b17bed
devodev <abalexandrebarone@gmail.com>
2020-07-18 10:30:51 -0400

    Import materialize-css and fontawesome, and use them in App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, Alex1! </h1>
+        {/* navbar */}
+        <div className="row no-margin-bottom red lighten-2 navbar">
+          <div className="col s4 m3 l2 red lighten-3 navbar-logo">
+            <i
+              className="fas fa-fan fa-spin"
+              style={{ verticalAlign: "middle" }}
+            ></i>
+            <span style={{ marginLeft: "20px", verticalAlign: "middle" }}>
+              Logo
+            </span>
+          </div>
+          <div className="col s8 m9 l10"></div>
+        </div>
+
+        {/* main */}
+        <div className="row no-margin-bottom main">
+          {/* left sidebar */}
+          <div className="col s4 m3 l2 teal lighten-2 sidebar"></div>
+
+          {/* content */}
+          <div className="col s8 m9 l10 teal darken-2 content"></div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
 import App from "./App.js";
+
 ReactDOM.render(<App />, document.getElementById("root"));
