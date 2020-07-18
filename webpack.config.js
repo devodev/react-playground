0c58c737638e973c607dbaa0cab638010e951db7
devodev <abalexandrebarone@gmail.com>
2020-07-18 10:30:25 -0400

    Add materialize-css and fontawesome libraries.
    Also add font and image loading in webpack config needed by fontawesome.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
     ],
   },
   resolve: {
